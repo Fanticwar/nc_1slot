016a6def4962d18151f6f6d17d7b5a1fd430e876
Fanticwar <78257179+Fanticwar@users.noreply.github.com>
2023-05-03 21:14:11 +0100

    Delete nc_1slot directory

--- nc_1slot/nc_player_hud/hints.lua
+++
@@ -1,8 +0,0 @@
--- LUALOCALS < ---------------------------------------------------------
-local nodecore
-    = nodecore
--- LUALOCALS > ---------------------------------------------------------
-
-nodecore.register_hint("hold your breath",
-	"breath_3"
-)
